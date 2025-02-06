125184b1616e561f1b1b88aa27a03948c2b2d7db
jash <javiercamarerolopez@gmail.com>
2025-02-06 12:29:46 +0100

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,35 @@
+#!/bin/bash
+
+# Actualizar el sistema
 sudo apt update
 
-sudo apt install nodejs cerbot
+# Instalar Certbot
+sudo apt install -y certbot
 
-npm install
+# Obtener certificados SSL para tu dominio
+sudo certbot certonly --standalone -d dev1.cyberbunny.online --non-interactive --agree-tos -m 17alma41@gmail.com
 
-cerbot certonly --standalone -d dev2.cyberbunny.online --non-interactive --agree-tos -m javiercamarerolopez@gmail.com
+# Crear directorio para almacenar los certificados
+mkdir -p message
 
-cp /etc/letsencrypt/live/cyberbunny.online/fullchain.pem .
-cp /etc/letsencrypt/live/cyberbunny.online/privkey.pem .
+# Copiar los certificados al directorio del proyecto
+sudo cp /etc/letsencrypt/live/dev1.cyberbunny.online/privkey.pem message/
+sudo cp /etc/letsencrypt/live/dev1.cyberbunny.online/fullchain.pem message/
+
+# Configurar tarea cron para renovar certificados automáticamente
+(crontab -l ; echo "0 0 * * * /usr/bin/certbot renew --quiet") | crontab -
+
+# Clonar el repositorio
+# git clone <URL_DEL_REPOSITORIO>
+
+# Entrar en el directorio del proyecto
+# cd <NOMBRE_DEL_REPOSITORIO>
+
+# Instalar dependencias
+# npm install
+
+# Iniciar el servidor
+npm run start
+
+# Comprobar que el servidor se ha iniciado correctamente
+curl -k https://dev1.cyberbunny.online:3000/
